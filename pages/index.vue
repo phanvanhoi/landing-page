91ee35a1331f23f6fdc8ffd65063f17538942325
Phan  Văn  Hội <hoi.phan@napaglobal.com>
2022-12-08 10:19:02 +0700

    init project

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Tutorial />
+    <h1>{{ $t("header.title") }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import i18n from "@/plugins/i18n";
+
+export default Vue.extend({
+  name: "IndexPage",
+});
+</script>
