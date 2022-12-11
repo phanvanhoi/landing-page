6b9ec163f56b902ebcff64e4e59091e0763708d9
phanvanhoi <35662892+phanvanhoi@users.noreply.github.com>
2022-12-11 21:03:58 +0700

    Responsive About component

--- components/About.vue
+++ components/About.vue
@@ -11,27 +11,30 @@
                     phổ biến nhất từ tất cả các nền tảng trò chơi điện tử phổ biến nhất, vì vậy bạn không cần phải tìm
                     kiếm niềm vui ở bất kỳ nơi nào khác trên Internet. Khám phá một thế giới trò chơi hoàn toàn mới trên
                     Etech.</h5>
-                <div class="number-of-people-container">
-                    <div class="number-of-people-content-container">
-                        <h1 class="number-of-people-h1">600</h1>
-                        <div class="number-of-people-h5-container">
-                            <h5 class="number-of-people-h5">M</h5>
+                <div class="number-of-people-main-container">
+                    <div class="number-of-people-container">
+                        <div class="number-of-people-content-container">
+                            <h1 class="number-of-people-h1">600</h1>
+                            <div class="number-of-people-h5-container">
+                                <h5 class="number-of-people-h5">{{ windowWidth > 576 ? 'M' : 'Triệu' }}</h5>
+                            </div>
+                            <div class="number-of-people-icon-container">
+                                <PlusIcon />
+                            </div>
                         </div>
-                        <div class="number-of-people-icon-container">
-                            <PlusIcon />
-                        </div>
+                        <h2 class="number-of-people-h2">Người chơi</h2>
                     </div>
-                    <h2 class="number-of-people-h2">Người chơi</h2>
-                </div>
-                <div class="number-of-people-container">
-                    <div class="number-of-people-content-container">
-                        <h1 class="number-of-people-h1">135</h1>
-                        <div class="number-of-people-icon-container">
-                            <PlusIcon />
+                    <div class="number-of-people-container">
+                        <div class="number-of-people-content-container">
+                            <h1 class="number-of-people-h1">135</h1>
+                            <div class="number-of-people-icon-container">
+                                <PlusIcon />
+                            </div>
                         </div>
+                        <h2 class="number-of-people-h2">Trò chơi</h2>
                     </div>
-                    <h2 class="number-of-people-h2">Trò chơi</h2>
                 </div>
+
             </div>
             <div class="flex-1 left-about-container">
                 <div class="left-about-padding-container">
@@ -103,6 +106,24 @@ export default defineComponent({
             current,
         };
     },
+    data() {
+        return {
+            windowWidth: window.innerWidth,
+        }
+    },
+    mounted() {
+        this.$nextTick(() => {
+            window.addEventListener('resize', this.onResize);
+        })
+    },
+    beforeDestroy() {
+        window.removeEventListener('resize', this.onResize);
+    },
+    methods: {
+        onResize() {
+            this.windowWidth = window.innerWidth
+        }
+    }
 });
 </script>
   
@@ -136,55 +157,60 @@ export default defineComponent({
                 font-weight: 400;
             }
 
-            .number-of-people-container {
+            .number-of-people-main-container {
                 display: flex;
-                justify-content: center;
                 flex-direction: column;
-                gap: calc(100vw * (1 / 24 / 4));
+                align-items: start;
 
-                .number-of-people-content-container {
+                .number-of-people-container {
                     display: flex;
-                    justify-content: start;
+                    justify-content: center;
+                    flex-direction: column;
+                    gap: calc(100vw * (1 / 24 / 4));
 
-                    .number-of-people-h1 {
-                        font-size: calc(100vw * (1 /20));
-                        line-height: calc(100vw * (1 /20));
-                        font-weight: bold;
-                        color: #079BEE;
-                        margin: 0;
-                    }
-
-                    .number-of-people-h5-container {
+                    .number-of-people-content-container {
                         display: flex;
-                        align-items: end;
+                        justify-content: start;
 
-                        .number-of-people-h5 {
-                            font-size: calc(100vw * (1 /20 / 2));
-                            line-height: calc(100vw * (1 /20 /1.75));
+                        .number-of-people-h1 {
+                            font-size: calc(100vw * (1 /20));
+                            line-height: calc(100vw * (1 /20));
                             font-weight: bold;
                             color: #079BEE;
                             margin: 0;
                         }
+
+                        .number-of-people-h5-container {
+                            display: flex;
+                            align-items: end;
+
+                            .number-of-people-h5 {
+                                font-size: calc(100vw * (1 /20 / 2));
+                                line-height: calc(100vw * (1 /20 /1.75));
+                                font-weight: bold;
+                                color: #079BEE;
+                                margin: 0;
+                            }
+                        }
+
+
+
+                        .number-of-people-icon-container {
+                            display: flex;
+                            align-items: center;
+
+                        }
+
                     }
 
-
-
-                    .number-of-people-icon-container {
-                        display: flex;
-                        align-items: center;
-
+                    .number-of-people-h2 {
+                        font-family: 'Montserrat';
+                        font-style: normal;
+                        font-weight: bold;
+                        font-size: calc(100vw * (1 / 24 / 3.5));
                     }
-
-                }
-
-                .number-of-people-h2 {
-                    font-family: 'Montserrat';
-                    font-style: normal;
-                    font-weight: bold;
-                    font-size: calc(100vw * (1 / 24 / 3.5));
                 }
             }
-
         }
 
         .left-about-container {
@@ -227,6 +253,144 @@ export default defineComponent({
         }
     }
 
+}
+
+@media only screen and (max-width: 576px) {
+    .about-container {
+        padding: 0 4vw;
+
+        .about-content-container {
+            flex-direction: column;
+
+            .flex-1 {
+                flex: 1;
+
+                .about-title-name {
+                    left: 0%;
+                    right: 0%;
+                    top: 0%;
+                    bottom: 87.58%;
+                    font-family: 'Playfair Display';
+                    font-style: normal;
+                    font-weight: 900;
+                    font-size: 40px;
+                    line-height: 50px;
+                    letter-spacing: 0.6px;
+                    color: #000000;
+                }
+
+                .about-descripstion {
+                    left: 0%;
+                    right: 0%;
+                    top: 15.53%;
+                    bottom: 26.5%;
+                    font-family: 'Montserrat';
+                    font-style: normal;
+                    font-weight: 400;
+                    font-size: 14px;
+                    line-height: 140%;
+
+                    color: #757575;
+                }
+
+                .number-of-people-main-container {
+                    flex-direction: row;
+                    justify-content: space-between;
+
+                    .number-of-people-container {
+                        display: flex;
+                        justify-content: center;
+                        flex-direction: column;
+                        gap: calc(100vw * (1 / 24 / 4));
+
+                        .number-of-people-content-container {
+                            display: flex;
+                            justify-content: start;
+
+                            .number-of-people-h1 {
+                                font-family: 'Playfair Display';
+                                font-style: normal;
+                                font-weight: 900;
+                                font-size: 40px;
+                                line-height: 50px;
+                            }
+
+                            .number-of-people-h5-container {
+                                display: flex;
+                                align-items: end;
+
+                                .number-of-people-h5 {
+                                    font-family: 'Playfair Display';
+                                    font-style: normal;
+                                    font-weight: 900;
+                                    font-size: 20px;
+                                    line-height: 40px;
+                                }
+                            }
+
+                            .number-of-people-icon-container {
+                                display: flex;
+                                align-items: center;
+
+                            }
+
+                        }
+
+                        .number-of-people-h2 {
+                            width: 164px;
+                            height: 30px;
+                            font-family: 'Montserrat';
+                            font-style: normal;
+                            font-weight: 700;
+                            font-size: 24px;
+                            line-height: 30px;
+                            color: #000000;
+                            flex: none;
+                            order: 1;
+                            flex-grow: 0;
+                            align-items: left;
+                        }
+                    }
+                }
+            }
+
+            .left-about-container {
+                background: #EEEEEE;
+
+                .left-about-padding-container {
+                    padding: calc(100vw * (1 / 17)) calc(100vw * (1 / 25.6));
+                    display: flex;
+                    flex-direction: column;
+                    gap: calc(100vw * (1 / 24 / 2));
+
+                    .left-about-content-container {
+                        display: flex;
+                        gap: 4vw;
+
+                        .about-icon-container {
+                            width: 10vw;
+                            height: 10vw;
+                            margin-top: 2vw;
+                        }
+
+                        .left-about-content-detail-container {
+                            flex: 1;
+
+                            h1 {
+                                font-weight: bold;
+                            }
+
+                            h5 {
+                                font-weight: 400;
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+
+    }
 }
 </style>
   
