ad0a777488a6027ae6d611b48523d5a85961f38c
phanvanhoi <35662892+phanvanhoi@users.noreply.github.com>
2022-12-11 22:53:17 +0700

    Responsive card game

--- components/CardList.vue
+++ components/CardList.vue
@@ -13,7 +13,7 @@
                 </div>
             </div>
             <div class="card-container" :style="{
-                'background-image': `url(${require('../static/Item1.png')})`,
+                'background-image': `url(${require('../static/Item2.png')})`,
             }">
                 <div class="description">
                     <h1>E-Space</h1>
@@ -24,7 +24,7 @@
                 </div>
             </div>
             <div class="card-container" :style="{
-                'background-image': `url(${require('../static/Item1.png')})`,
+                'background-image': `url(${require('../static/Item3.png')})`,
             }">
                 <div class="description">
                     <h1>E-Space</h1>
@@ -34,10 +34,45 @@
                         dolore magna aliqua.</h5>
                 </div>
             </div>
+            <!-- start -->
+            <div v-show="windowWidth <= 576" class="card-container" :style="{
+                'background-image': `url(${require('../static/Item4.png')})`,
+            }">
+                <div class="description">
+                    <h1>E-Space</h1>
+                    <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                        et
+                        dolore magna aliqua.</h5>
+                </div>
+            </div>
+            <div v-show="windowWidth <= 576" class="card-container" :style="{
+                'background-image': `url(${require('../static/Item5.png')})`,
+            }">
+                <div class="description">
+                    <h1>E-Space</h1>
+                    <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                        et
+                        dolore magna aliqua.</h5>
+                </div>
+            </div>
+            <div v-show="windowWidth <= 576" class="card-container" :style="{
+                'background-image': `url(${require('../static/Item6.png')})`,
+            }">
+                <div class="description">
+                    <h1>E-Space</h1>
+                    <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                        et
+                        dolore magna aliqua.</h5>
+                </div>
+            </div>
+            <!-- end -->
         </div>
         <div class="column-card-container item-old">
             <div class="card-container" :style="{
-                'background-image': `url(${require('../static/Item1.png')})`,
+                'background-image': `url(${require('../static/Item7.png')})`,
             }">
                 <div class="description">
                     <h1>E-Space</h1>
@@ -48,7 +83,7 @@
                 </div>
             </div>
             <div class="card-container" :style="{
-                'background-image': `url(${require('../static/Item1.png')})`,
+                'background-image': `url(${require('../static/Item8.png')})`,
             }">
                 <div class="description">
                     <h1>E-Space</h1>
@@ -59,7 +94,77 @@
                 </div>
             </div>
             <div class="card-container" :style="{
-                'background-image': `url(${require('../static/Item1.png')})`,
+                'background-image': `url(${require('../static/Item9.png')})`,
+            }">
+                <div class="description">
+                    <h1>E-Space</h1>
+                    <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                        et
+                        dolore magna aliqua.</h5>
+                </div>
+            </div>
+            <!-- start -->
+            <div v-show="windowWidth <= 576" class="card-container" :style="{
+                'background-image': `url(${require('../static/Item10.png')})`,
+            }">
+                <div class="description">
+                    <h1>E-Space</h1>
+                    <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                        et
+                        dolore magna aliqua.</h5>
+                </div>
+            </div>
+            <div v-show="windowWidth <= 576" class="card-container" :style="{
+                'background-image': `url(${require('../static/Item11.png')})`,
+            }">
+                <div class="description">
+                    <h1>E-Space</h1>
+                    <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                        et
+                        dolore magna aliqua.</h5>
+                </div>
+            </div>
+            <div v-show="windowWidth <= 576" class="card-container" :style="{
+                'background-image': `url(${require('../static/Item12.png')})`,
+            }">
+                <div class="description">
+                    <h1>E-Space</h1>
+                    <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                        et
+                        dolore magna aliqua.</h5>
+                </div>
+            </div>
+            <!-- end -->
+        </div>
+        <div v-show="windowWidth > 576" class="column-card-container">
+            <div class="card-container" :style="{
+                'background-image': `url(${require('../static/Item4.png')})`,
+            }">
+                <div class="description">
+                    <h1>E-Space</h1>
+                    <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                        et
+                        dolore magna aliqua.</h5>
+                </div>
+            </div>
+            <div class="card-container" :style="{
+                'background-image': `url(${require('../static/Item5.png')})`,
+            }">
+                <div class="description">
+                    <h1>E-Space</h1>
+                    <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                        et
+                        dolore magna aliqua.</h5>
+                </div>
+            </div>
+            <div class="card-container" :style="{
+                'background-image': `url(${require('../static/Item6.png')})`,
             }">
                 <div class="description">
                     <h1>E-Space</h1>
@@ -70,9 +175,9 @@
                 </div>
             </div>
         </div>
-        <div class="column-card-container">
+        <div v-show="windowWidth > 576" class="column-card-container item-old">
             <div class="card-container" :style="{
-                'background-image': `url(${require('../static/Item1.png')})`,
+                'background-image': `url(${require('../static/Item10.png')})`,
             }">
                 <div class="description">
                     <h1>E-Space</h1>
@@ -83,7 +188,7 @@
                 </div>
             </div>
             <div class="card-container" :style="{
-                'background-image': `url(${require('../static/Item1.png')})`,
+                'background-image': `url(${require('../static/Item11.png')})`,
             }">
                 <div class="description">
                     <h1>E-Space</h1>
@@ -94,42 +199,7 @@
                 </div>
             </div>
             <div class="card-container" :style="{
-                'background-image': `url(${require('../static/Item1.png')})`,
-            }">
-                <div class="description">
-                    <h1>E-Space</h1>
-                    <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore
-                        et
-                        dolore magna aliqua.</h5>
-                </div>
-            </div>
-        </div>
-        <div class="column-card-container item-old">
-            <div class="card-container" :style="{
-                'background-image': `url(${require('../static/Item1.png')})`,
-            }">
-                <div class="description">
-                    <h1>E-Space</h1>
-                    <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore
-                        et
-                        dolore magna aliqua.</h5>
-                </div>
-            </div>
-            <div class="card-container" :style="{
-                'background-image': `url(${require('../static/Item1.png')})`,
-            }">
-                <div class="description">
-                    <h1>E-Space</h1>
-                    <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                        labore
-                        et
-                        dolore magna aliqua.</h5>
-                </div>
-            </div>
-            <div class="card-container" :style="{
-                'background-image': `url(${require('../static/Item1.png')})`,
+                'background-image': `url(${require('../static/Item12.png')})`,
             }">
                 <div class="description">
                     <h1>E-Space</h1>
@@ -162,8 +232,22 @@ export default defineComponent({
         const current = ref<string[]>(['mail']);
         return {
             current,
+            windowWidth: window.innerWidth,
         };
     },
+    mounted() {
+        this.$nextTick(() => {
+            window.addEventListener('resize', this.onResize);
+        })
+    },
+    beforeDestroy() {
+        window.removeEventListener('resize', this.onResize);
+    },
+    methods: {
+        onResize() {
+            this.windowWidth = window.innerWidth
+        }
+    }
 });
 </script>
   
@@ -202,6 +286,14 @@ export default defineComponent({
 
                 h5 {
                     color: white;
+                    font-family: 'Montserrat';
+                    font-style: normal;
+                    font-weight: 400;
+                    font-size: 14px;
+                    line-height: 140%;
+                    display: flex;
+                    align-items: flex-end;
+                    color: #FFFFFF;
                 }
             }
 
@@ -210,6 +302,55 @@ export default defineComponent({
 
     .item-old {
         margin-top: calc(100vw / 12);
+    }
+
+}
+
+@media only screen and (max-width: 576px) {
+    .container {
+        display: flex;
+        width: 100%;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        align-content: space-between;
+
+        .column-card-container {
+            width: 45vw;
+            display: flex;
+            flex-direction: column;
+            gap: calc(100vw * (1 / 24 /2));
+
+            .card-container {
+                width: 100%;
+                height: 70vw;
+                border-radius: 12px;
+                box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3);
+                background-size: cover;
+                display: flex;
+                align-items: end;
+                padding: calc(100vw / 24 / 2.7);
+
+
+
+                .description {
+
+                    h1 {
+                        color: white;
+                        margin-bottom: 10px;
+                    }
+
+                    h5 {
+                        color: white;
+                    }
+                }
+
+            }
+        }
+
+        .item-old {
+            margin-top: calc(100vw / 12);
+        }
+
     }
 
 }
--- components/Game.vue
+++ components/Game.vue
@@ -63,9 +63,47 @@ export default defineComponent({
 
         .about-descripstion {
             font-size: calc(100vw * (1 /137.142857143));
-            margin-bottom: calc(100vw * (1 / 24 / 2));
+            margin-bottom: calc(100vw * (1 / 24));
             font-weight: 400;
             text-align: center;
+        }
+    }
+}
+
+@media only screen and (max-width: 576px) {
+    .games-container {
+
+        .description-header {
+            width: 100%;
+
+            .about-title-name {
+                left: 4.27%;
+                right: 4.27%;
+                top: 0%;
+                bottom: 96.99%;
+                font-family: 'Playfair Display';
+                font-style: normal;
+                font-weight: 900;
+                font-size: 40px;
+                line-height: 50px;
+                text-align: center;
+                letter-spacing: 0.6px;
+                color: #000000;
+            }
+
+            .about-descripstion {
+                left: 4.27%;
+                right: 4.27%;
+                top: 3.76%;
+                bottom: 89.22%;
+                font-family: 'Montserrat';
+                font-style: normal;
+                font-weight: 400;
+                font-size: 14px;
+                line-height: 140%;
+                text-align: center;
+                color: #757575;
+            }
         }
     }
 }
