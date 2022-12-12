453b0cb44200920591422fe0ea3fdfbb2e6d0c0d
phanvanhoi <35662892+phanvanhoi@users.noreply.github.com>
2022-12-12 07:17:16 +0700

    Responsive partner

--- components/CountDown.vue
+++ components/CountDown.vue
@@ -2,7 +2,7 @@
     <div id="countdown-container-component">
         <vue-countdown-timer id="vue-countdown-timer" @start_callback="startCallBack('event started')"
             @end_callback="endCallBack('event ended')" :start-time="'2022-12-09 00:00:00'"
-            :end-time="'2022-12-12 00:00:00'" :interval="1000" label-position="begin" :end-text="'Event ended!'"
+            :end-time="'2023-22-01 00:00:00'" :interval="1000" label-position="begin" :end-text="'Event ended!'"
             :day-txt="'days'" :hour-txt="'hours'" :minutes-txt="'minutes'" :seconds-txt="'seconds'">
             <!-- <template slot="start-label" slot-scope="scope">
                 <span style="color: red"
